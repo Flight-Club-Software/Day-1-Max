3d253e3c8545d7ec69f96b7ce9d48153edfd5a13
getwebbed <66648999+getwebbed@users.noreply.github.com>
2020-10-29 20:58:33 -0700

    Create test.c
    First C thing, think its wrong somehow.

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    printStars();
+}
+
+int printStars() {
+    int count = 5; 
+    for (count = 5; count >=0; count--);
+    printf("*");
+}
+ return 0;
